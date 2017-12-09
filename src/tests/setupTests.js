53aca1b8656d47b2c2b3c957098d98e5245a844a
Toreilly317 <Taylor@TaylorOReilly.com>
2017-12-08 20:41:42 -0500

    upgrade react to v16

--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -1,1 +1,10 @@
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import DotEnv from 'dotenv'
+
+Enzyme.configure({
+    adapter: new Adapter()
+})
+
+
 require('dotenv').config({ path: '.env.test' });
